eab97d18190afdfd394f5d6685c4ffcd7e7705f0
jbdura <bidurajohnson@gmail.com>
2022-09-27 08:56:05 +0300

    commit tree 5e86e74760a1584f176f3bbf45f751668a917dee

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,13 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  
+  def show
+    # Find cheese using ID
+    # byebug
+    cheese = Cheese.find_by(id: params[:id])
+    # send a JSON response using cheese object
+    render json: cheese
+  end
 
 end
